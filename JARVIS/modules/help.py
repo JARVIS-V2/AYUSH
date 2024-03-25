4320ac7248a7b1af1bf95a99223d93e375a98060
🇯𝙰𝚁𝚅𝙸𝚂 <161505946+JARVIS-V2@users.noreply.github.com>
2024-03-25 14:20:02 +0530

    Update help.py

--- JARVIS/modules/help.py
+++ JARVIS/modules/help.py
@@ -3,7 +3,7 @@ from telethon import events, Button
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
 
 
-HELP_STRING = f"★ 𓆩🜲𓆪KILLER𓆩🜲𓆪 ʙᴏᴛs ʜᴇʟᴘ ᴍᴇɴᴜ ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @OFFICIAL_KILLER1**"
+HELP_STRING = f"★ 𓆩ꨄ𓆪-GOD_AYUSH_OP🕊⃝‌ٖٖ🦋 ʙᴏᴛs ʜᴇʟᴘ ᴍᴇɴᴜ ★\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @GOD_AYUSH_OP**"
 
 HELP_BUTTON = [
     [
@@ -14,7 +14,7 @@ HELP_BUTTON = [
       Button.inline("• ᴇxᴛʀᴀ •", data="extra")
     ],
     [
-      Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/+IWE4jiSU3sUwNmQ1"),
+      Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/JARVIS_X_SUPPORT"),
       Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/CHATTING_2024")
     ]
   ]
@@ -60,7 +60,7 @@ extra_msg = f"""
   2) {hl}leave : Type in the Group bot will auto leave that group
 
 
-**© @OFFICIAL_KILLER1**
+**© @GOD_AYUSH_OP**
 """
 
                  
@@ -92,7 +92,7 @@ raid_msg = f"""
   2) {hl}craid <count> <reply to user>
 
 
-**© @OFFICIAL_KILLER1**
+**© @GOD_AYUSH_OP**
 """
 
 spam_msg = f"""
@@ -109,7 +109,7 @@ spam_msg = f"""
   1) {hl}hang <counter>
 
 
-** © @OFFICIAL_KILLER1**
+** © @GOD_AYUSH_OP**
 """                     
            
            
@@ -142,7 +142,7 @@ async def helpback(event):
             ]
           )
     else:
-        await event.answer("ᴘᴀʜʟᴇ 𓆩🜲𓆪KILLER𓆩🜲𓆪 ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍!! @OFFICIAL_KILLER1", cache_time=0, alert=True)
+        await event.answer("ᴘᴀʜʟᴇ 𓆩ꨄ𓆪-GOD_AYUSH_OP🕊⃝‌ٖٖ🦋 ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍!! @GOD_AYUSH_OP", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"spam"))
@@ -161,7 +161,7 @@ async def help_spam(event):
               buttons=[[Button.inline("< Back", data="help_back"),],],
               ) 
     else:
-        await event.answer("ᴘᴀʜʟᴇ 𓆩🜲𓆪KILLER𓆩🜲𓆪 ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍 !! @OFFICIAL_KILLER1", cache_time=0, alert=True)
+        await event.answer("ᴘᴀʜʟᴇ 𓆩ꨄ𓆪-GOD_AYUSH_OP🕊⃝‌ٖٖ🦋 ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍 !! @GOD_AYUSH_OP", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"raid"))
@@ -180,7 +180,7 @@ async def help_raid(event):
             buttons=[[Button.inline("< Back", data="help_back"),],],
           )
     else:
-        await event.answer("ᴘᴀʜʟᴇ 𓆩🜲𓆪KILLER𓆩🜲𓆪 ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍 !! @OFFICIAL_KILLER1", cache_time=0, alert=True)
+        await event.answer("ᴘᴀʜʟᴇ 𓆩ꨄ𓆪-GOD_AYUSH_OP🕊⃝‌ٖٖ🦋 ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍 !! @GOD_AYUSH_OP", cache_time=0, alert=True)
 
 
 @X1.on(events.CallbackQuery(pattern=r"extra"))
@@ -199,5 +199,5 @@ async def help_extra(event):
             buttons=[[Button.inline("< Back", data="help_back"),],],
             )
     else:
-        await event.answer("ᴘᴀʜʟᴇ 𓆩🜲𓆪KILLER𓆩🜲𓆪 ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍!! @OFFICIAL_KILLER1", cache_time=0, alert=True)
+        await event.answer("ᴘᴀʜʟᴇ 𓆩ꨄ𓆪-GOD_AYUSH_OP🕊⃝‌ٖٖ🦋 ᴘᴀᴘᴀ sᴇ sᴜᴅᴏ ʟᴇʟᴏ 𖤍!! @GOD_AYUSH_OP", cache_time=0, alert=True)
 
